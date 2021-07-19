97e110735595d47d6c39dc8c2b507ca78d644700
Don Oh <okdonguk0@gmail.com>
2021-07-18 22:32:23 -0600

    Create Java_arraylist.java

---
+++ HackerRank/src/easy/Java_arraylist.java
@@ -0,0 +1,12 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Java_arraylist {
+	public static void main (String[] agrs) {
+		Scanner inp = new Scanner(System.in);
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		inp.close();
+    }
+}
