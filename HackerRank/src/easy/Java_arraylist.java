647e1701f94fc396293c60451fa1820b619b76e7
Don Oh <okdonguk0@gmail.com>
2021-07-19 22:33:36 -0600

    Update Java_arraylist.java

--- HackerRank/src/easy/Java_arraylist.java
+++ HackerRank/src/easy/Java_arraylist.java
@@ -7,6 +7,7 @@ public class Java_arraylist {
 	public static void main (String[] agrs) {
 		Scanner inp = new Scanner(System.in);
 		ArrayList<Integer> arr = new ArrayList<Integer>();
+		
 		inp.close();
     }
 }
